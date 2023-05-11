e51c16a7dbb5039a061054dc07325bb08cc35cbc
Falcón <luis_adrian359@comunidad.unam.mx>
2023-05-11 00:09:15 -0600

    Instrucciones y Jugar, error en audio

---
+++ DuckHunt/src/fes/aragon/controller/InicioController.java
@@ -0,0 +1,80 @@
+package fes.aragon.controller;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class InicioController {
+
+    @FXML
+    private Button btnCreditos;
+
+    @FXML
+    private Button btnInstruciones;
+
+    @FXML
+    private Button btnJugar;
+
+    @FXML
+    private Text txtTitulo;
+
+    
+    
+    
+    
+    @FXML
+    void creditos(ActionEvent event) {
+    	System.out.println("Credits");
+    	this.nuevaVentana("Creditos");
+    	this.cerrarVentana(btnCreditos);
+    }
+
+    @FXML
+    void instrucciones(ActionEvent event) {
+    	System.out.println("Instrucciones");
+    	this.nuevaVentana("Instrucciones");
+    	this.cerrarVentana(btnInstruciones);
+    }
+
+    @FXML
+    void jugar(ActionEvent event) {
+    	System.out.println("JUGAR");
+    	this.nuevaVentana("Juego");
+    	this.cerrarVentana(btnJugar);
+    }
+    
+    public void nuevaVentana(String archivo) {
+		try  {
+			Pane root = (Pane) FXMLLoader.load(getClass().getResource("/fes/aragon/fxml/" + archivo + ".fxml"));
+			Scene scene = new Scene(root);
+			Stage escenario = new Stage();
+			escenario.setScene(scene);
+//			escenario.initStyle(StageStyle.UNDECORATED);
+			escenario.initModality(Modality.APPLICATION_MODAL);
+			escenario.show();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+    
+    public void cerrarVentana(Button boton) {
+		Stage stage = (Stage) boton.getScene().getWindow();
+		stage.close();
+	}
+	
+    
+    
+    
+
+}
---
+++ DuckHunt/src/fes/aragon/controller/JuegoController.java
@@ -0,0 +1,104 @@
+package fes.aragon.controller;
+
+import fes.aragon.extras.MusicaCiclica;
+import fes.aragon.modelo.Disparos;
+import fes.aragon.modelo.Enemigos;
+import fes.aragon.modelo.Fondo;
+import fes.aragon.modelo.Nave;
+import javafx.animation.AnimationTimer;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyEvent;
+
+public class JuegoController {
+
+	private Scene escena;
+	private GraphicsContext graficos;
+//	private Enemigos enemigos;
+//	private Fondo fondo;
+//	private Nave nave;
+//	private Disparos disparos;
+	private Thread hiloFondo;
+	
+    @FXML
+    private Canvas canvas;
+    
+    
+    public void iniciar() {
+		componentesIniciar();
+		
+		pintar();
+		eventosTeclado();
+		ciclo();
+	}
+    
+    private void componentesIniciar() {
+		graficos = canvas.getGraphicsContext2D();
+		MusicaCiclica entrada = new MusicaCiclica("musica_entrada");
+		hiloFondo = new Thread(entrada);
+		hiloFondo.start();
+		enemigos = new Enemigos(20, 20, null, 1);
+		fondo=new Fondo(0, 0,"/fes/aragon/resource/fondo/img (1).gif" , 4,24);
+		nave=new Nave(20,255,"/fes/aragon/resource/navefinal.png",2);
+		nave.setrEnemigo(enemigos.getEnemigos());
+		disparos=new Disparos(0, 0, null, 2);
+		nave.setDisparos(disparos);
+		
+	}
+    
+    public void ciclo() {
+		long tiempoInicio = System.nanoTime();
+		
+		AnimationTimer tiempo = new AnimationTimer() {
+			@Override
+			public void handle(long tiempoActual) {
+				double t = (tiempoActual - tiempoInicio) / 1000000000.00;
+				fondo.setTiempo(t);
+				calculosLogica();
+				pintar();
+
+			}
+
+		};
+		tiempo.start();
+	}
+    
+    private void eventosTeclado() {
+		escena.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent arg0) {				
+				// TODO Auto-generated method stub
+				nave.teclado(arg0,true);
+				
+			}			
+		});
+		escena.setOnKeyReleased(new EventHandler<KeyEvent>() {
+
+			@Override
+			public void handle(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				nave.teclado(arg0,false);
+			
+			}
+			
+		});
+		
+	}
+    
+private void pintar() {
+		
+		this.fondo.pintar(graficos);
+		this.nave.pintar(graficos);
+		this.enemigos.pintar(graficos);
+		this.disparos.pintar(graficos);
+		
+	}
+    
+    public void setEscena(Scene escena) {
+		this.escena = escena;
+		
+	}
+}
